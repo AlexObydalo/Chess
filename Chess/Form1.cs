613b3a426a428b02c412e6c671919e3aa3bf8a79
Alexandr Obydalo <oskar9x9@gmail.com>
2021-05-21 21:39:01 +0300

    Добавлена проверка на шах. Если король под ударом он подсвечиваеться красным.

--- Chess/Form1.cs
+++ Chess/Form1.cs
@@ -370,6 +370,13 @@ namespace Chess
                         button4.Visible = false;//Кнопка "ход вперед" НЕ видна
                     }
 
+                    ReDrawMap(); //Перерисовать
+                    
+                    if (IsThrereCheck(map))//Если есть шах
+                    {
+                        MakeOurKingRed();//Сделать клетку короля красной
+                    }
+
                     
                     
                     
@@ -812,6 +819,11 @@ namespace Chess
             
             button4.Visible = true;//Кнопка "ход вперед" видна
 
+            
+            if (IsThrereCheck(map))//Если есть шах
+            {
+                MakeOurKingRed();//Сделать клетку короля красной
+            }
 
         }
 
@@ -847,6 +859,11 @@ namespace Chess
 
             button3.Visible = true; //Кнопка "ход назад" видна
 
+
+            if (IsThrereCheck(map))//Если есть шах
+            {
+                MakeOurKingRed();//Сделать клетку короля красной
+            }
         }
 
         public void ShowChooseButtonsForPawns() //Показать кнопки превращений пешки
@@ -957,7 +974,7 @@ namespace Chess
         }
 
 
-        private void CastleClick(object sender, EventArgs e)
+        private void CastleClick(object sender, EventArgs e)//Превращение пешки в ладью
         {
             map[PawnI, PawnJ] = (1 + currPlayer % 2) * 10 + 5; //Запись пешки как ладьи
             ActivateAllButtons(); //Активировать все кнопки
@@ -975,7 +992,7 @@ namespace Chess
             Controls.Remove(Queen);
         }
 
-        private void HourseClick(object sender, EventArgs e)
+        private void HourseClick(object sender, EventArgs e)//Превращение пешки в коня
         {
             map[PawnI, PawnJ] = (1 + currPlayer%2) * 10 + 4; //Запись пешки как коня
             ActivateAllButtons(); //Активировать все кнопки
@@ -993,7 +1010,7 @@ namespace Chess
             Controls.Remove(Queen);
         }
 
-        private void OfficerClick(object sender, EventArgs e)
+        private void OfficerClick(object sender, EventArgs e)//Превращение пешки в офицера
         {
             map[PawnI, PawnJ] = (1 + currPlayer % 2) * 10 + 3; //Запись пешки как офицера
             ActivateAllButtons(); //Активировать все кнопки
@@ -1011,7 +1028,7 @@ namespace Chess
             Controls.Remove(Queen);
         }
 
-        private void QueenClick(object sender, EventArgs e)
+        private void QueenClick(object sender, EventArgs e)//Превращение пешки в ферзя
         {
             map[PawnI, PawnJ] = (1 + currPlayer % 2) * 10 + 2; //Запись пешки как королевы
             ActivateAllButtons(); //Активировать все кнопки
@@ -1028,6 +1045,367 @@ namespace Chess
             Controls.Remove(Officer);
             Controls.Remove(Queen);
         }
+
+
+
+        public bool IsThrereCheck(int[,] desk)//Проверка на наличие шаха. Метод принимает доску (двумерный массив)
+        {
+            List<int> Figures = ReturnAllAttackedFigures(desk); //Записать все фигуры которые под ударом в массив
+            foreach(int f in Figures)//Перебор фигур под ударом
+            {
+                if(f == currPlayer*10 + 1)//Если фигура под ударом - наш король
+                {
+                    return true; //Вернуть true
+                }
+            }
+            return false;//Если король не обнаружен - вернуть false.
+        }
+
+        public List<int> ReturnAllAttackedFigures(int[,] desk)//Обнаружение отакованых фигур. Метод принимает доску (двумерный массив)
+        {
+            List<int> AllAtackedF = new List<int>(); //Все атакованные фигуры
+            //Перебор клеток на доске
+            for (int i = 0; i<8; i++) 
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if(desk[i,j]/10 == 1+(currPlayer%2)) //Если на клетке вражеская фигура
+                    {
+                        switch(desk[i, j]%10)
+                        {
+                            case 6://Для пешек
+                                
+                                foreach (int f in ReturnFiguresAttackedByPawn(i, j, desk))//Перебор фигур под ударом пешки
+                                {
+                                    AllAtackedF.Add(f); //Добавить фигуру в атакованные
+                                }
+
+                                break;
+                            case 5://Для ладей
+                                foreach (int f in ReturnFiguresAttackedVerticalHorizontal(i, j, desk))//Перебор фигур под ударом ладьи
+                                {
+                                    AllAtackedF.Add(f); //Добавить фигуру в атакованные
+                                }
+
+                                break;
+                            case 4://Для коней
+                                foreach (int f in ReturnFiguresAttackedHorseSteps(i, j, desk))//Перебор фигур под ударом коня
+                                {
+                                    AllAtackedF.Add(f); //Добавить фигуру в атакованные
+                                }
+
+                                break;
+                            case 3://Для офицеров
+                                foreach (int f in ReturnFiguresAttackedDiagonal(i, j, desk))//Перебор фигур под ударом офицера
+                                {
+                                    AllAtackedF.Add(f); //Добавить фигуру в атакованные
+                                }
+                                break;
+                            case 2://Для ферзей
+                                foreach (int f in ReturnFiguresAttackedVerticalHorizontal(i, j, desk))//Перебор фигур под вертикальным или горизонтальным ударом
+                                {
+                                    AllAtackedF.Add(f); //Добавить фигуру в атакованные
+                                }
+                                foreach (int f in ReturnFiguresAttackedDiagonal(i, j, desk))//Перебор фигур под косым ударом
+                                {
+                                    AllAtackedF.Add(f); //Добавить фигуру в атакованные
+                                }
+                                break;
+                            case 1://Для королей
+                                foreach (int f in ReturnFiguresAttackedVerticalHorizontal(i, j, desk, true))//Перебор фигур под вертикальным или горизонтальным ударом на один ход
+                                {
+                                    AllAtackedF.Add(f); //Добавить фигуру в атакованные
+                                }
+                                foreach (int f in ReturnFiguresAttackedDiagonal(i, j, desk, true))//Перебор фигур под косым ударом на один ход
+                                {
+                                    AllAtackedF.Add(f); //Добавить фигуру в атакованные
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+
+            return AllAtackedF; //Вернуть все атакованые фигуры
+        }
+
+        public List<int> ReturnFiguresAttackedByPawn(int IcurrFigure, int JcurrFigure, int[,] desk)//Обнаружение фигур отакованых пешкой. 
+        {
+            List<int> AttackedF = new List<int>(); //Список атакованных фигур
+            
+            int dir = currPlayer == 1 ? -1 : 1; //(если играют белые  dir = -1, иначе dir = 1) dir переменная по направлению пешки. 
+            
+            if (InsideBorder(IcurrFigure + 1 * dir, JcurrFigure + 1)) //Находиться ли правый атакующий (косой) ход пешкой в пределах доски
+            {
+                if (desk[IcurrFigure + 1 * dir, JcurrFigure + 1] != 0 && desk[IcurrFigure + 1 * dir, JcurrFigure + 1] / 10 == currPlayer) //Два условия: клетка не пуста, фигура на ней - наша
+                {
+                    AttackedF.Add(desk[IcurrFigure + 1 * dir, JcurrFigure + 1]); //Записать фигуру в список
+                }
+            }
+            if (InsideBorder(IcurrFigure + 1 * dir, JcurrFigure - 1)) ////Находиться ли левый атакующий (косой) ход пешкой в пределах доски
+            {
+                if (desk[IcurrFigure + 1 * dir, JcurrFigure - 1] != 0 && desk[IcurrFigure + 1 * dir, JcurrFigure - 1] / 10 == currPlayer) //Два условия: клетка не пуста, фигура на ней - наша
+                {
+                    AttackedF.Add(desk[IcurrFigure + 1 * dir, JcurrFigure - 1]); //Записать фигуру в список
+                }
+            }
+            return AttackedF; //Вернуть список атакованных фигур
+        }
+
+        public List<int> ReturnFiguresAttackedVerticalHorizontal(int IcurrFigure, int JcurrFigure, int[,] desk, bool isOneStep = false)//Обнаружение фигур отакованых по вертикальной прямой
+        {
+            List<int> AttackedF = new List<int>(); //Список атакованных фигур
+
+            for (int i = IcurrFigure + 1; i < 8; i++) //Движение вверх
+            {
+                if (InsideBorder(i, JcurrFigure)) //Клетка в пределах доски
+                {
+                    if(desk[i,JcurrFigure]!=0) //Если клетка не пуста
+                    {
+                        if(desk[i,JcurrFigure]/10 == currPlayer) //если отакована наша фигура 
+                        {
+                            AttackedF.Add(desk[i, JcurrFigure]); //Записать отакованную фигуру
+                        }
+                        break;
+                    }
+                }
+                if (isOneStep) //Если у фигуры только один ход
+                    break;
+            }
+            
+            for (int i = IcurrFigure - 1; i >= 0; i--) //Движение вниз
+            {
+                if (InsideBorder(i, JcurrFigure)) //Клетка в пределах доски
+                {
+                    if (desk[i, JcurrFigure] != 0) //Если клетка не пуста
+                    {
+                        if (desk[i, JcurrFigure] / 10 == currPlayer) //если отакована наша фигура 
+                        {
+                            AttackedF.Add(desk[i, JcurrFigure]); //Записать отакованную фигуру
+                        }
+                        break;
+                    }
+                }
+                if (isOneStep) //Если у фигуры только один ход
+                    break;
+            }
+            
+            for (int j = JcurrFigure + 1; j < 8; j++) //Движение вправо
+            {
+                if (InsideBorder(IcurrFigure, j)) //Клетка в пределах доски
+                {
+                    if (desk[IcurrFigure, j] != 0) //Если клетка не пуста
+                    {
+                        if (desk[IcurrFigure, j] / 10 == currPlayer) //если отакована наша фигура 
+                        {
+                            AttackedF.Add(desk[IcurrFigure, j]); //Записать отакованную фигуру
+                        }
+                        break;
+                    }
+                }
+                if (isOneStep) //Если у фигуры только один ход
+                    break;
+            }
+            
+            for (int j = JcurrFigure - 1; j >= 0; j--) //Движение влево
+            {
+                if (InsideBorder(IcurrFigure, j)) //Клетка в пределах доски
+                {
+                    if (desk[IcurrFigure, j] != 0) //Если клетка не пуста
+                    {
+                        if (desk[IcurrFigure, j] / 10 == currPlayer) //если отакована наша фигура 
+                        {
+                            AttackedF.Add(desk[IcurrFigure, j]); //Записать отакованную фигуру
+                        }
+                        break;
+                    }
+                }
+                if (isOneStep) //Если у фигуры только один ход
+                    break;
+
+                
+            }
+            
+            return AttackedF; //Отправить список атакованных фигур
+        }
+
+        public List<int> ReturnFiguresAttackedDiagonal(int IcurrFigure, int JcurrFigure, int [,] desk, bool isOneStep = false) ////Обнаружение фигур отакованых по косой линии
+        {
+            List<int> AttackedF = new List<int>(); //Список атакованных фигур
+
+            int j = JcurrFigure + 1;
+            for (int i = IcurrFigure - 1; i >= 0; i--)
+            {
+                if (InsideBorder(i, j))
+                {
+                    if (desk[i, j] != 0) //Если клетка не пуста
+                    {
+                        if (desk[i, j] / 10 == currPlayer) //если отакована наша фигура 
+                        {
+                            AttackedF.Add(desk[i, j]); //Записать отакованную фигуру
+                        }
+                        break;
+                    }
+                }
+                if (j < 7)
+                    j++;
+                else break;
+
+                if (isOneStep)
+                    break;
+            }
+
+            j = JcurrFigure - 1;
+            for (int i = IcurrFigure - 1; i >= 0; i--)
+            {
+                if (InsideBorder(i, j))
+                {
+                    if (desk[i, j] != 0) //Если клетка не пуста
+                    {
+                        if (desk[i, j] / 10 == currPlayer) //если отакована наша фигура 
+                        {
+                            AttackedF.Add(desk[i, j]); //Записать отакованную фигуру
+                        }
+                        break;
+                    }
+                }
+                if (j > 0)
+                    j--;
+                else break;
+
+                if (isOneStep)
+                    break;
+            }
+
+            j = JcurrFigure - 1;
+            for (int i = IcurrFigure + 1; i < 8; i++)
+            {
+                if (InsideBorder(i, j))
+                {
+                    if (desk[i, j] != 0) //Если клетка не пуста
+                    {
+                        if (desk[i, j] / 10 == currPlayer) //если отакована наша фигура 
+                        {
+                            AttackedF.Add(desk[i, j]); //Записать отакованную фигуру
+                        }
+                        break;
+                    }
+                }
+                if (j > 0)
+                    j--;
+                else break;
+
+                if (isOneStep)
+                    break;
+            }
+
+            j = JcurrFigure + 1;
+            for (int i = IcurrFigure + 1; i < 8; i++)
+            {
+                if (InsideBorder(i, j))
+                {
+                    if (desk[i, j] != 0) //Если клетка не пуста
+                    {
+                        if (desk[i, j] / 10 == currPlayer) //если отакована наша фигура 
+                        {
+                            AttackedF.Add(desk[i, j]); //Записать отакованную фигуру
+                        }
+                        break;
+                    }
+                }
+                if (j < 7)
+                    j++;
+                else break;
+
+                if (isOneStep)
+                    break;
+            }
+
+            return AttackedF; //Вернуть отакованные фигуры
+
+
+        }
+
+        public List<int> ReturnFiguresAttackedHorseSteps(int IcurrFigure, int JcurrFigure, int[,] desk) //Ходы коня
+        {
+            List<int> AttackedF = new List<int>();
+            
+            if (InsideBorder(IcurrFigure - 2, JcurrFigure + 1)) //Клетка в пределах доски
+            {
+                if (desk[IcurrFigure-2, JcurrFigure + 1] / 10 == currPlayer) //если отакована наша фигура 
+                {
+                    AttackedF.Add(desk[IcurrFigure - 2, JcurrFigure + 1]); //Записать отакованную фигуру
+                }
+            }
+            if (InsideBorder(IcurrFigure - 2, JcurrFigure - 1)) //Клетка в пределах доски
+            {
+                if (desk[IcurrFigure - 2, JcurrFigure - 1] / 10 == currPlayer) //если отакована наша фигура 
+                {
+                    AttackedF.Add(desk[IcurrFigure - 2, JcurrFigure - 1]); //Записать отакованную фигуру
+                }
+            }
+            if (InsideBorder(IcurrFigure + 2, JcurrFigure + 1)) //Клетка в пределах доски
+            {
+                if (desk[IcurrFigure + 2, JcurrFigure + 1] / 10 == currPlayer) //если отакована наша фигура 
+                {
+                    AttackedF.Add(desk[IcurrFigure + 2, JcurrFigure + 1]); //Записать отакованную фигуру
+                }
+            }
+            if (InsideBorder(IcurrFigure + 2, JcurrFigure - 1)) //Клетка в пределах доски
+            {
+                if (desk[IcurrFigure + 2, JcurrFigure - 1] / 10 == currPlayer) //если отакована наша фигура 
+                {
+                    AttackedF.Add(desk[IcurrFigure + 2, JcurrFigure - 1]); //Записать отакованную фигуру
+                }
+            }
+            if (InsideBorder(IcurrFigure - 1, JcurrFigure + 2)) //Клетка в пределах доски
+            {
+                if (desk[IcurrFigure - 1, JcurrFigure + 2] / 10 == currPlayer) //если отакована наша фигура 
+                {
+                    AttackedF.Add(desk[IcurrFigure - 1, JcurrFigure + 2]); //Записать отакованную фигуру
+                }
+            }
+            if (InsideBorder(IcurrFigure + 1, JcurrFigure + 2)) //Клетка в пределах доски
+            {
+                if (desk[IcurrFigure + 1, JcurrFigure + 2] / 10 == currPlayer) //если отакована наша фигура 
+                {
+                    AttackedF.Add(desk[IcurrFigure + 1, JcurrFigure + 2]); //Записать отакованную фигуру
+                }
+            }
+            if (InsideBorder(IcurrFigure - 1, JcurrFigure - 2)) //Клетка в пределах доски
+            {
+                if (desk[IcurrFigure - 1, JcurrFigure - 2] / 10 == currPlayer) //если отакована наша фигура 
+                {
+                    AttackedF.Add(desk[IcurrFigure - 1, JcurrFigure - 2]); //Записать отакованную фигуру
+                }
+            }
+            if (InsideBorder(IcurrFigure + 1, JcurrFigure - 2)) //Клетка в пределах доски
+            {
+                if (desk[IcurrFigure + 1, JcurrFigure - 2] / 10 == currPlayer) //если отакована наша фигура 
+                {
+                    AttackedF.Add(desk[IcurrFigure + 1, JcurrFigure - 2]); //Записать отакованную фигуру
+                }
+            }
+
+            return AttackedF; //Вернуть атакованые фигуры
+        }
+
+        public void MakeOurKingRed()//Сделать нашего короля красным
+        {
+            //Перебор клеток на доске
+            for(int i = 0; i<8; i++)
+            {
+                for (int j=0; j < 8; j++)
+                {
+                    if(map[i,j]==currPlayer*10+1)//Если на клетке наш король
+                    {
+                        butts[i, j].BackColor = Color.Red; //Покрасить кнопку в красный
+                    }
+                }
+            }
+        }
+
+
 
         private void label1_Click(object sender, EventArgs e)
         {
